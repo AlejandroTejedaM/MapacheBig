122cdc5b4afe14158a8bd3782ecdf9fe3fd2b2e7
Kintx2227 <alex2227@hotmail.com>
2024-09-15 12:56:54 -0600

    Added methods for find a user by email and password

--- API/appointment/src/main/java/com/barber/appointment/Repository/UserRepository.java
+++ API/appointment/src/main/java/com/barber/appointment/Repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.barber.appointment.Repository;
 import com.barber.appointment.Model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
-
+    Optional<User> findByCorreo(String correo);
+    Optional<User> findByCorreoAndContrasennia(String correo, String contrasennia);
 }
-
