a81d97a73d0f1b6e07fe951135a223a6c8e32a61
Kintx2227 <alex2227@hotmail.com>
2024-09-15 12:21:50 -0600

    Added disponibilidad model

---
+++ API/appointment/src/main/java/com/barber/appointment/Model/Availability.java
@@ -0,0 +1,88 @@
+package com.barber.appointment.Model;
+
+import jakarta.persistence.*;
+
+import java.sql.Time;
+
+@Entity
+@Table(name = "availability")
+public class Availability {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long disponibilidadId;
+
+    @Enumerated(EnumType.STRING)
+    private DiasSemana diaSemana;
+
+    @Column(nullable = false)
+    private Time horaInicio;
+
+    @Column(nullable = false)
+    private Time horaFin;
+
+    @ManyToOne
+    @JoinColumn(name = "usuarioId", nullable = false, referencedColumnName = "usuarioId")
+    private User user;
+
+    public Availability() {
+    }
+
+    public Availability(DiasSemana diaSemana, Time horaInicio, Time horaFin, User user) {
+        this.diaSemana = diaSemana;
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
+        this.user = user;
+    }
+
+    public Long getDisponibilidadId() {
+        return disponibilidadId;
+    }
+
+    public void setDisponibilidadId(Long disponibilidadId) {
+        this.disponibilidadId = disponibilidadId;
+    }
+
+    public DiasSemana getDiaSemana() {
+        return diaSemana;
+    }
+
+    public void setDiaSemana(DiasSemana diaSemana) {
+        this.diaSemana = diaSemana;
+    }
+
+    public Time getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(Time horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public Time getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(Time horaFin) {
+        this.horaFin = horaFin;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Availability{" +
+                "disponibilidadId=" + disponibilidadId +
+                ", diaSemana=" + diaSemana +
+                ", horaInicio=" + horaInicio +
+                ", horaFin=" + horaFin +
+                ", user=" + user +
+                '}';
+    }
+}
